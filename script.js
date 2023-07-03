c049c058ec142ed9f36c3c1b73ef84b0b99b3858
RajbeerChandra <100615997+RajbeerChandra@users.noreply.github.com>
2023-07-03 23:54:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".multiple-text", {
+      strings: ["Web Developer", "Video Editor","Android Developer"],
+      typeSpeed: 100,
+      typeSpeed: 100,
+      typeSpeed: 100,
+      loop: true,
+      loopCount: Infinity,
+      showCursor: false,
+      autoInsertCss: true,
+    });
+  });
